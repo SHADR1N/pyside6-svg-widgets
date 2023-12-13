bba8f94cb7b296cadc8048c13bd5ca07318b8dda
shadr1n <s.h.a.d.r.i.n.mvp@gmail.com>
2023-12-13 18:24:41 +0400

    Commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyside6-svg-widgets',
+    version='0.0.1',
+    author='SHADRIN',
+    author_email='none@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PySide6 svg widgets',
+    url='https://github.com/SHADR1N/pyside6-svg-widgets.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PySide6>=6.4.0',
+    ]
+)
