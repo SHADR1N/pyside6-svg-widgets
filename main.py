c458463830c2d3e9a1a826fd65e648db85979338
shadr1n <s.h.a.d.r.i.n.mvp@gmail.com>
2024-02-22 22:18:00 +0400

    Update sample code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 try:
     from .pyside6_svg_widgets import QSvgButton, QIconSvg, QDropButton
 except ImportError:
-    from pyside6_svg_widgets import QSvgButton, QIconSvg, QDropButton, QSvgButtonIcon
+    from pyside6_svg_widgets import QSvgButton, QIconSvg, QDropButton, QSvgButtonIcon, SVGRender
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QApplication
 from PySide6.QtCore import Qt
@@ -15,7 +15,7 @@ STYLE_WIDGET = """
     background: #1E293B;
     font: 12pt;
 }
-QSvgButton, QIconSvg, QSvgButtonIcon {
+SVGRender, QIconSvg, QSvgButtonIcon {
     padding:  5px;
     padding-left: 10px;
     padding-right: 10px;
@@ -23,23 +23,23 @@ QSvgButton, QIconSvg, QSvgButtonIcon {
     border: 0px solid transparent;
     border-radius: 5%;
     color: #fff;
-    icon-color: #fff;
+/*  icon-color: #fff; */
 }
 
-QSvgButton:hover, QSvgButtonIcon:hover, QIconSvg:hover {
+SVGRender:hover, QSvgButtonIcon:hover, QIconSvg:hover {
     padding:  5px;
     padding-left: 10px;
     padding-right: 10px;
     color: #496EF6;
-    icon-color: red;
+/*  icon-color: #496EF6; */
     background: #344254;
 }
-QSvgButton:pressed, QIconSvg:pressed {
+SVGRender:pressed, QIconSvg:pressed {
     padding:  5px;
     padding-left: 10px;
     padding-right: 10px;
     color: #3276C3;
-    icon-color: #3276C3;
+    /* icon-color: #3276C3; */
 }
 """
 
@@ -53,9 +53,9 @@ class SvgButtonExample(QWidget):
         self.setStyleSheet(STYLE_WIDGET)
 
     def __initUi(self):
-        svg = QSvgButtonIcon()
+        img = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-open-dot"><path d="m6 14 1.45-2.9A2 2 0 0 1 9.24 10H20a2 2 0 0 1 1.94 2.5l-1.55 6a2 2 0 0 1-1.94 1.5H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h3.93a2 2 0 0 1 1.66.9l.82 1.2a2 2 0 0 0 1.66.9H18a2 2 0 0 1 2 2v2"/><circle cx="14" cy="15" r="1"/></svg>'
+        svg = SVGRender(svg_string=img, size_ic=(30, 30))
         svg.setObjectName(u"svgWidget")
-        svg.setSvg("tasks.svg")
         svg.clicked.connect(lambda: print("clicked"))
 
         # newButton = QSvgButton()
